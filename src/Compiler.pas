79d24e1fd57731a78af01fba2b80c1c8cc66f138
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-09-13 17:18:18 -0400

    Implement translations for simple commands

--- src/Compiler.pas
+++ src/Compiler.pas
@@ -12,6 +12,34 @@ unit Compiler;
   uses
     PathUtils;
 
+  function CompileCD(const Line: string): string;
+  var
+    SplitLoc: Integer;
+  begin
+    SplitLoc := Pos('cd ', Line) + 3;
+    Result := Copy(Line, SplitLoc, MaxInt);
+  end;
+
+  function CompileEcho(const Line: string): string;
+  var
+    SplitLoc: Integer;
+  begin
+    //TODO: handle arguments
+    Result := 'echo ' + Copy(Line, 5, MaxInt); //split after 'echo '
+  end;
+
+  function CompileExit(const Line: string): string;
+  begin
+    if Length(Line) > 5 then
+    begin
+      Result := 'exit /B'; //don't want to close terminal window
+    end
+    else //Has exit code
+    begin
+      Result := 'exit /B ' + Copy(Line, 6, MaxInt);
+    end;
+  end;
+
   procedure Compile(const FileName: string; const OutputPath:string);
   var
     FileData: TextFile;
@@ -37,7 +65,58 @@ unit Compiler;
         Rewrite(OutputFile);
         try
           Readln(FileData, Line);
-          WriteLn(OutputFile, Line);
+
+          if Line = ':' then //Null Instruction
+          begin
+            writeln(OutputFile, ':');
+          end
+          //TODO: running seperate file (. filename)
+          //TODO: bind
+          //TODO: builtin
+          else if Pos('cd ', Line) > 0 then //want space after to ensure it is the command
+          begin
+            writeln(OutputFile, CompileCD(Line));
+          end
+          //TODO: command
+          //TODO: continue
+          //TODO: declare
+          else if Pos('echo', Line) > 0 then
+          begin
+            writeln(OutputFile, CompileEcho(Line));
+          end
+          //TODO: enable
+          //TODO: eval
+          //TODO: exec
+          else if Pos('exit ', Line) > 0 then
+          begin
+            writeln(OutputFile, CompileExit(Line));
+          end
+          //TODO: getopts
+          //TODO: hash
+          //TODO: help
+          //TODO: let
+          //TODO: local
+          //TODO: logout
+          //TODO: printf
+          else if Pos('pwd', Line) > 0 then //ignore symbolic links args
+          begin
+            writeln(OutputFile, 'echo %cd%'); 
+          end
+          //TODO: read
+          //TODO: readonly
+          //TODO: return
+          //TODO: set
+          //TODO: shift
+          //TODO: shopt
+          //TODO: source
+          //TODO: type
+          //TODO: typeset
+          //TODO: test
+          //TODO: times
+          //TODO: trap
+          //TODO: ulimit
+          //TODO: unmask
+          //TODO: unset
         finally
           CloseFile(OutputFile);
         end;
