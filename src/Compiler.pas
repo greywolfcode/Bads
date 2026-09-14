191e68b480c5f3e3bb5c8ee213a29b5e36f56df0
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-09-13 21:20:21 -0400

    Refactor compiler Separates file reading and bat string generation

--- src/Compiler.pas
+++ src/Compiler.pas
@@ -40,6 +40,68 @@ unit Compiler;
     end;
   end;
 
+
+
+  function CompileLine(Line: string): string;
+  var
+    OutputLine: string;
+  begin
+    OutputLine := '';
+
+    if Line = ':' then //Null Instruction
+    begin
+      OutputLine := ':';
+    end
+    //TODO: running seperate file (. filename)
+    //TODO: bind
+    //TODO: builtin
+    else if Pos('cd ', Line) > 0 then //want space after to ensure it is the command
+    begin
+      OutputLine :=  CompileCD(Line);
+    end
+    //TODO: command
+    //TODO: continue
+    //TODO: declare
+    else if Pos('echo', Line) > 0 then
+    begin
+      OutputLine :=  CompileEcho(Line);
+    end
+    //TODO: enable
+    //TODO: eval
+    //TODO: exec
+    else if Pos('exit ', Line) > 0 then
+    begin
+      OutputLine :=  CompileExit(Line);
+    end
+    //TODO: getopts
+    //TODO: hash
+    //TODO: help
+    //TODO: let
+    //TODO: local
+    //TODO: logout
+    //TODO: printf
+    else if Pos('pwd', Line) > 0 then //ignore symbolic links args
+    begin
+      OutputLine := 'echo %cd%';
+    end;
+    //TODO: read
+    //TODO: readonly
+    //TODO: return
+    //TODO: set
+    //TODO: shift
+    //TODO: shopt
+    //TODO: source
+    //TODO: type
+    //TODO: typeset
+    //TODO: test
+    //TODO: times
+    //TODO: trap
+    //TODO: ulimit
+    //TODO: unmask
+    //TODO: unset
+    Result := OutputLine;
+  end;
+  
   procedure Compile(const FileName: string; const OutputPath:string);
   var
     FileData: TextFile;
@@ -64,59 +126,8 @@ unit Compiler;
         AssignFile(OutputFile, OutputFileName);
         Rewrite(OutputFile);
         try
-          Readln(FileData, Line);
-
-          if Line = ':' then //Null Instruction
-          begin
-            writeln(OutputFile, ':');
-          end
-          //TODO: running seperate file (. filename)
-          //TODO: bind
-          //TODO: builtin
-          else if Pos('cd ', Line) > 0 then //want space after to ensure it is the command
-          begin
-            writeln(OutputFile, CompileCD(Line));
-          end
-          //TODO: command
-          //TODO: continue
-          //TODO: declare
-          else if Pos('echo', Line) > 0 then
-          begin
-            writeln(OutputFile, CompileEcho(Line));
-          end
-          //TODO: enable
-          //TODO: eval
-          //TODO: exec
-          else if Pos('exit ', Line) > 0 then
-          begin
-            writeln(OutputFile, CompileExit(Line));
-          end
-          //TODO: getopts
-          //TODO: hash
-          //TODO: help
-          //TODO: let
-          //TODO: local
-          //TODO: logout
-          //TODO: printf
-          else if Pos('pwd', Line) > 0 then //ignore symbolic links args
-          begin
-            writeln(OutputFile, 'echo %cd%'); 
-          end
-          //TODO: read
-          //TODO: readonly
-          //TODO: return
-          //TODO: set
-          //TODO: shift
-          //TODO: shopt
-          //TODO: source
-          //TODO: type
-          //TODO: typeset
-          //TODO: test
-          //TODO: times
-          //TODO: trap
-          //TODO: ulimit
-          //TODO: unmask
-          //TODO: unset
+          readln(FileData, Line);
+          writeln(OutputFile, CompileLine(Line));
         finally
           CloseFile(OutputFile);
         end;
@@ -124,7 +135,6 @@ unit Compiler;
     finally
       CloseFile(FileData);
     end;
-
   end;
-
+  
 end.
