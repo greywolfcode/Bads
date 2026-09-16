ed35d2a054d411ba3c294517b02a3e04468294d7
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-09-15 20:23:02 -0400

    Create base for Transpiler

---
+++ src/Transpiler.pas
@@ -0,0 +1,16 @@
+unit Transpiler;
+
+  interface
+
+  uses
+    TokenData;
+
+  procedure Transpile(Tokens: TTokenArray);
+
+  implementation
+
+  procedure Transpile(Tokens: TTokenArray);
+  begin
+  end;
+
+end.
